2c4f9d810473b3f74a2892b4823ec1a3e0b07062
salvatore-ferrone <salvatore.ferrone.1996@gmail.com>
2025-07-14 20:43:20 +0200

    adjust dt

--- demos/numericalError/starParticlesErr1.py
+++ demos/numericalError/starParticlesErr1.py
@@ -1,3 +1,7 @@
+"""
+This modules contains functions to study the numerical error in the integration of star particles in a static Plummer potential.
+"""
+
 import numpy as np
 import matplotlib.pyplot as plt
 import matplotlib as mpl
@@ -8,32 +12,39 @@ import multiprocessing as mp
 
 
 
-def main(NP = 100, integrationtime = 10, alpha = 1/100):
-    """
-    Main function to run the star particle integration with error analysis.
-    
-    Parameters:
-    NP : int
-        Number of particles to simulate.
-    integrationtime : float
-        Total time for the integration in dynamical time units.
-    alpha : float
-        Time step scaling factor relative to the dynamical time.
-    """
-    
-    initialkinematics, staticgalaxy, integrationparameters = prepare_integration_arguments(NP, integrationtime, alpha)
-    
-    # integrate the orbit
-    stream_orbit, finalpositions = leapfrogintime(initialkinematics, staticgalaxy, integrationparameters)
-    
-    # calculate the relative energy error
-    Erel = relative_energy_error_orbit(initialkinematics, staticgalaxy, stream_orbit)
-    
-    return stream_orbit, finalpositions, Erel
-
-
 
 #### HELPER FUNCTIONS ####
+
+
+def adjust_dt_factor(alpha, tau, integrationtime):
+    """
+    Adjust the time step based on the dynamical time and the scaling factor.
+    This ensures that the integration interval is evenly divisible by the time step.
+    And that the factor to create the time step is less than or equal to the user provided alpha.
+    This is to ensure that the time step is a fraction of the dynamical time.
+    
+    Parameters:    alpha : float
+        Time step scaling factor relative to the dynamical time to make the time step
+    tau : float
+        Dynamical time of the system.
+    integrationtime : float
+        Total time for the integration in dynamical time units.
+    Returns:
+    float
+        Adjusted time step.
+    """
+
+    dt_provisoire = alpha * tau
+    assert (dt_provisoire) < integrationtime, "alpha * tau must be less than integrationtime"
+    
+    # get the number of dividions of the interval
+    k = np.log2(integrationtime / alpha / tau ) + 1
+    # round up to ensure that alpha_out is lower than alpha and evenly divides the integration time
+    k = np.ceil(k)
+    NSTEP = int(2**(k-1))
+    alpha_adjusted = integrationtime / 2**(k-1) / tau
+    return alpha_adjusted, NSTEP
+
 
 def prepare_integration_arguments(NP = 100, integrationtime = 10, alpha = 1/100):
 
@@ -43,7 +54,9 @@ def prepare_integration_arguments(NP = 100, integrationtime = 10, alpha = 1/100)
     aplum = tstrippy.ergodic.convertHalfMassRadiusToPlummerRadius(a)
     tdyn = np.sqrt(a**3/(G*M))
     dt = alpha * tdyn
-    NSTEP = int(integrationtime/dt)
+
+    alpha_adjusted, NSTEP = adjust_dt_factor(alpha, tdyn, integrationtime)    
+    dt = alpha_adjusted * tdyn
 
     # sample the plummer sphere
     xp,yp,zp,vxp,vyp,vzp = tstrippy.ergodic.isotropicplummer(G,M,a,NP)
@@ -111,8 +124,10 @@ def realative_enery_error_final(initialkinematics, staticgalaxy, stream):
 
 ##### INTEGRATION FUNCTIONS #####
 
-def leapfrogintime(initialkinematics, staticgalaxy, integrationparameters):
-
+def leapfrogintime(args):
+    
+    initialkinematics, staticgalaxy, integrationparameters = args
+    
     NSTEP = integrationparameters[2]
     NP = len(initialkinematics[0])
     
@@ -130,7 +145,7 @@ def leapfrogintime(initialkinematics, staticgalaxy, integrationparameters):
     
     return stream_orbit, np.array([xf, yf, zf, vxf, vyf, vzf])
 
-def leapfrogtofinalpositions(initialkinematics, staticgalaxy, integrationparameters):
+def leapfrogtofinalpositions(args):
     """
     Main function to run the star particle integration with error analysis.
     
@@ -142,6 +157,8 @@ def leapfrogtofinalpositions(initialkinematics, staticgalaxy, integrationparamet
     alpha : float
         Time step scaling factor relative to the dynamical time.
     """
+    
+    initialkinematics, staticgalaxy, integrationparameters = args
     
     tstrippy.integrator.deallocate()
     tstrippy.integrator.setinitialkinematics(*initialkinematics)
@@ -155,6 +172,35 @@ def leapfrogtofinalpositions(initialkinematics, staticgalaxy, integrationparamet
     return stream
 
     
+
+def batch_leapfrogtofinalpositions(args, nbatches, freecpu = 2 ):
+    """
+    Use multiprocessing to run the leapfrog integration in batches.
+    """
     
+    
+    integrationparameters,staticgalaxy,initialkinematics = args
+    NP = initialkinematics[0].shape[0]
+    ncpu = mp.cpu_count() - freecpu
+    NP_per_batch = NP // nbatches
+    assert NP_per_batch > 0, "Number of particles is too small for the number of batches"
+    args = []
+    for i in range(nbatches):
+        start = i * NP_per_batch
+        end = (i + 1) * NP_per_batch if i < nbatches - 1 else NP
+        args.append((integrationparameters, staticgalaxy, initialkinematics[:, start:end]))
+    
+    print(f"Running {nbatches} batches with {ncpu} CPUs, each with {NP_per_batch} particles")
+    with mp.Pool(ncpu) as pool:
+        results = pool.map(leapfrogtofinalpositions, args)
+    
+    # Combine the results
+    stream = np.zeros((6, NP))
+    compTime = []
+    for i, result in enumerate(results):
+        start = i * NP_per_batch
+        end = (i + 1) * NP_per_batch if i < nbatches - 1 else NP
+        stream[:, start:end] = result[0]
+        compTime.append(result[1])
 
-
+    return stream, compTime
